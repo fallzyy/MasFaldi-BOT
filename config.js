a873104cfa40d0ba9044edbc27955a7f351f32c3
fallzyy <143869327+fallzyy@users.noreply.github.com>
2023-10-04 14:56:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['6281291974700', 'me', true],
+  ['6281291974700', 'MasFaldi', true], 
   [''] 
 ] //Number of owners
 
